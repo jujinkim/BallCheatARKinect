f7d988713402ad34ee37a1059f31dba84fd347d1
김주진 <jk012345@gmail.com>
2016-06-18 23:57:43 +0900

    Add many functions. Add Output Perspective wrap controller. Add Kinect tilt adjust controller. Add ball detection. Etc.

--- BallCheatARKinect/MainWindow.xaml.cs
+++ BallCheatARKinect/MainWindow.xaml.cs
@@ -43,6 +43,7 @@ namespace BallCheatARKinect
         Image<Bgr, byte> imgColor;
         Image<Bgr, byte> imgCue;
         Image<Bgr, byte> imgDisplay;
+        Image<Gray, byte> imgBallGray;
         #endregion
 
         #region private variables
@@ -55,19 +56,21 @@ namespace BallCheatARKinect
         int ballSizeMin = 2;
         int ballSizeMax = 10;
 
-        int ballDrawingRadiusMultiply = 2;  //출력할 때 공 동그라미 그리는거 크기 반지름배수
+        int ballDrawingRadiusMultiply = 20;  //출력할 때 공 동그라미 그리는거 크기 반지름배수
 
         bool isPoolDepthSetting = true;
         bool isBallDepthSetting = true;
         int poolPosSetting = 1;
 
-        Point[] poolPos = new Point[2]; //Region of Interest
-        int poolWidth, poolHeight;  //poolPos 설정시에 자동으로 설정
+        System.Drawing.Point[] poolPos = new System.Drawing.Point[2]; //Region of Interest
+        int poolWidth = 640, poolHeight = 480;  //poolPos 설정시에 자동으로 설정
 
-        int blobCount = 0;
-        
+        System.Drawing.PointF[] srcs = new System.Drawing.PointF[4];
+        System.Drawing.PointF[] dest = new System.Drawing.PointF[4];
+
+        int ballCount = 0;
+        HomographyMatrix mywarpmat;
         #endregion
-
 
         public MainWindow()
         {
@@ -80,6 +83,7 @@ namespace BallCheatARKinect
             foreach(var potentialSensor in KinectSensor.KinectSensors)
                 if(potentialSensor.Status == KinectStatus.Connected)
                 {
+     
                     sensor = potentialSensor;
                     break;
                 }
@@ -98,29 +102,55 @@ namespace BallCheatARKinect
                 //SetDisplay();
 
                 sensor.AllFramesReady += sensor_AllFramesReady;
-
+                
                 try
-                {
-                    sensor.Start();
-
-                }
+                { sensor.Start(); }
                 catch (IOException ee)
                 {
                     log(ee.Message);
                     sensor = null;
                 }
+           
+                //Kinect Tilt
+                //sldKinectTilt.Value = sensor.ElevationAngle;
+                sldKinectTilt.Maximum = sensor.MaxElevationAngle;
+                sldKinectTilt.Minimum = sensor.MinElevationAngle;
+                CvInvoke.cvNamedWindow("Display");
+
+                poolPos[0] = new System.Drawing.Point(0, 0);
+                poolPos[1] = new System.Drawing.Point(0, 0);
+
+                //output warping point setting
+                srcs[0] = new System.Drawing.PointF(0, 0);
+                srcs[1] = new System.Drawing.PointF(640, 0);
+                srcs[2] = new System.Drawing.PointF(640, 480);
+                srcs[3] = new System.Drawing.PointF(0, 480);
+                srcs.CopyTo(dest, 0);
+
+                //image initialize
+                imgDisplay = new Image<Bgr, byte>(poolWidth, poolHeight, new Bgr(System.Drawing.Color.Black));
+                imgDepth = new Image<Bgr, byte>(640, 480);
+                imgBall = new Image<Bgr, byte>(640, 480);
+                imgColor = new Image<Bgr, byte>(640, 480);
+                imgBallGray = new Image<Gray, byte>(640, 480);
+
+                mywarpmat = CameraCalibration.GetPerspectiveTransform(srcs, dest);
+
             }
 
-            if(sensor == null)
+            if (sensor == null)
             {
                 log("Kinect Connect Fail");
                 pnlSetting.IsEnabled = false;
+                pnlSetting2.IsEnabled = false;
             }
-
-            CvInvoke.cvNamedWindow("Display");
         }
 
-
+        /// <summary>
+        /// Kinect All Frames Ready Update callback method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
         {
             bool isCalculate = !isPoolDepthSetting && !isBallDepthSetting && (poolPosSetting == 0);
@@ -129,17 +159,61 @@ namespace BallCheatARKinect
             {
                 using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
                 {
+                    //Clear imgDisplay
+                    imgDisplay.Resize(poolWidth, poolHeight, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR);
+                    imgDisplay.SetZero();
+                    //imgDisplay.SetZero();
+                    imgDisplay.Draw(new System.Drawing.Rectangle(0, 0, imgDisplay.Width, imgDisplay.Height), new Bgr(System.Drawing.Color.White), 3);
+
                     if (depthFrame != null)
                     {
                         depthFrame.CopyDepthImagePixelDataTo(depthPixels);
+
+                        ballCount = 0;
+
                         depthBitmap.WritePixels(
                             new Int32Rect(0, 0, this.depthBitmap.PixelWidth, this.depthBitmap.PixelHeight),
                             this.depthPixels, this.depthBitmap.PixelWidth * sizeof(int), 0);
-                        imgDepth = new Image<Bgr, byte>(depthBitmap.ToBitmap());
+                        imgDepth.Bitmap = depthBitmap.ToBitmap();
 
-                        if(isCalculate)
-                        imgBall = new Image<Bgr, byte>(depthFrame.SliceDepthImage(ballDepth - ballDepthRange, ballDepth + ballDepthRange).ToBitmap());
-                        
+                        //Calculate (all settings are done)
+                        if (isCalculate)
+                        {
+                            imgBall.Bitmap = depthFrame.SliceDepthImage(ballDepth - ballDepthRange, ballDepth + ballDepthRange).ToBitmap();
+
+                            imgBallGray = imgBall.Convert<Gray, byte>();
+                            //CvInvoke.cvCvtColor(imgBall, imgBallGray, Emgu.CV.CvEnum.COLOR_CONVERSION.BGR2GRAY);
+                            imgBallGray.ROI = new System.Drawing.Rectangle((int)poolPos[0].X, (int)poolPos[0].Y, poolWidth, poolHeight);
+
+                            //imgBall labeling
+                            using (MemStorage stor = new MemStorage())
+                            {
+                                //Find contours with no holes try CV_RETR_EXTERNAL to find holes
+                                Contour<System.Drawing.Point> contours = imgBallGray.FindContours(
+                                 Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE,
+                                 Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_EXTERNAL, stor);
+
+                                for (int i = 0; contours != null; contours = contours.HNext)
+                                {
+                                    i++;
+
+                                    if ((contours.Area > Math.Pow(ballSizeMin, 2)) && (contours.Area < Math.Pow(ballSizeMax, 2)))
+                                    {
+                                        //Draw balls
+                                        MCvBox2D box = contours.GetMinAreaRect();
+                                        //CvInvoke.cvCircle(imgBall, new System.Drawing.Point((int)box.center.X, (int)box.center.Y), ballDrawingRadiusMultiply, new MCvScalar(0, 0, 255), 2, Emgu.CV.CvEnum.LINE_TYPE.FOUR_CONNECTED, 0);
+                                        imgBall.Draw(new CircleF(box.center, ballDrawingRadiusMultiply), new Bgr(System.Drawing.Color.Blue), 2);
+                                        System.Drawing.Point ballPos = new System.Drawing.Point((int)(box.center.X - poolPos[0].X), (int)(box.center.Y - poolPos[0].Y));
+                                        imgDisplay.Draw(new CircleF(ballPos, ballDrawingRadiusMultiply), new Bgr(System.Drawing.Color.White), 1);
+                                        //CvInvoke.cvCircle(imgDisplay, ballPos, ballDrawingRadiusMultiply, new MCvScalar(255, 255, 255), 2, Emgu.CV.CvEnum.LINE_TYPE.FOUR_CONNECTED, 0);
+                                        ballCount++;
+                                    }
+                                }
+                            }   //end labeling
+                            lblSttBallCountVal.Text = ballCount.ToString();
+
+                            
+                        }   //End Calculate
                     }
                 }
 
@@ -150,7 +224,11 @@ namespace BallCheatARKinect
                     this.colorBitmap.WritePixels(
                         new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
                         this.colorPixels, this.colorBitmap.PixelWidth * sizeof(int), 0);
-                    imgColor = new Image<Bgr, byte>(colorBitmap.ToBitmap());
+                    imgColor.Bitmap = colorBitmap.ToBitmap();
+
+                    //draw pool position dot
+                    //CvInvoke.cvCircle(imgColor, poolPos[0] )
+                    //imgColor.Draw(new CircleF(new System.Drawing.PointF((float)poolPos[0].X, (float)poolPos[0].Y), 1), new Bgr(System.Drawing.Color.Red), 1);
 
                     //draw pool position setting guideline
                     if (!isPoolDepthSetting && !isBallDepthSetting)
@@ -160,23 +238,29 @@ namespace BallCheatARKinect
                             {
                                 if (lblPoolPos1Val.Text.IndexOf(',') > 0)
                                 {
-                                    Point mP = Point.Parse(lblPoolPos1Val.Text);
-                                    CvInvoke.cvLine(imgColor, new System.Drawing.Point((int)mP.X, 0), new System.Drawing.Point((int)mP.X, 480), new MCvScalar(100, 100, 100), 1, Emgu.CV.CvEnum.LINE_TYPE.FOUR_CONNECTED, 0);
-                                    CvInvoke.cvLine(imgColor, new System.Drawing.Point(0, (int)mP.Y), new System.Drawing.Point(640, (int)mP.Y), new MCvScalar(100, 100, 100), 1, Emgu.CV.CvEnum.LINE_TYPE.FOUR_CONNECTED, 0);
-                                }
+                                    System.Windows.Point mP = System.Windows.Point.Parse(lblPoolPos1Val.Text);
+                                    System.Drawing.PointF mPF = new System.Drawing.PointF((float)mP.X, (float)mP.Y);
+                                    imgColor.Draw(new Cross2DF(mPF, 1280, 960), new Bgr(System.Drawing.Color.LightGreen), 1);
+                                 }
                             }
                             else if (poolPosSetting == 2)
                             {
                                 if (lblPoolPos2Val.Text.IndexOf(',') > 0)
                                 {
-                                    Point mP = Point.Parse(lblPoolPos2Val.Text);
-                                    CvInvoke.cvLine(imgColor, new System.Drawing.Point((int)mP.X, 0), new System.Drawing.Point((int)mP.X, 480), new MCvScalar(100, 100, 100), 1, Emgu.CV.CvEnum.LINE_TYPE.FOUR_CONNECTED, 0);
-                                    CvInvoke.cvLine(imgColor, new System.Drawing.Point(0, (int)mP.Y), new System.Drawing.Point(640, (int)mP.Y), new MCvScalar(100, 100, 100), 1, Emgu.CV.CvEnum.LINE_TYPE.FOUR_CONNECTED, 0);
+                                    System.Windows.Point mP = System.Windows.Point.Parse(lblPoolPos2Val.Text);
+                                    System.Drawing.PointF mPF = new System.Drawing.PointF((float)mP.X, (float)mP.Y);
+                                    imgColor.Draw(new Cross2DF(mPF, 1280, 960), new Bgr(System.Drawing.Color.LightGreen), 1);
                                 }
                             }
                         }
                         catch { }
 
+                    if(isCalculate)
+                    {
+                        //Draw pool area rectangle
+                        imgColor.Draw(new System.Drawing.Rectangle(poolPos[0].X, poolPos[0].Y, poolWidth, poolHeight), new Bgr(System.Drawing.Color.White), 2);
+                        //CvInvoke.cvRectangle(imgColor, poolPos[0], poolPos[1], new MCvScalar(255, 255, 255), 1, Emgu.CV.CvEnum.LINE_TYPE.FOUR_CONNECTED, 0);
+                    }
                 }
 
 
@@ -186,7 +270,9 @@ namespace BallCheatARKinect
                 SetDisplay();
             else
                 imgOutMain.Source = null;
-            CvInvoke.cvShowImage("Display", imgColor);
+
+            //imgDisplay = imgDisplay.WarpPerspective(mywarpmat, Emgu.CV.CvEnum.INTER.CV_INTER_NN, Emgu.CV.CvEnum.WARP.CV_WARP_FILL_OUTLIERS, new Bgr(0, 0, 0));
+            CvInvoke.cvShowImage("Display", imgDisplay);// imgDisplay.WarpPerspective(mywarpmat, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR, Emgu.CV.CvEnum.WARP.CV_WARP_DEFAULT, new Bgr(0, 0, 0)));
 
         }
 
@@ -199,7 +285,7 @@ namespace BallCheatARKinect
             {
                 case DisplayImg.TOTAL: imgOutMain.Source = ImageHelpers.ToBitmapSource(imgColor); break;
                 case DisplayImg.BALL: imgOutMain.Source = ImageHelpers.ToBitmapSource(imgBall); break;
-                case DisplayImg.DEPTH: imgOutMain.Source = depthBitmap; break;//ImageHelpers.ToBitmapSource(imgDepth); break;
+                case DisplayImg.DEPTH: imgOutMain.Source = depthBitmap; break;// ImageHelpers.ToBitmapSource(imgDepth); break;
                 case DisplayImg.CUE: imgOutMain.Source = ImageHelpers.ToBitmapSource(imgCue); break;
                 case DisplayImg.DISPLAY: imgOutMain.Source = ImageHelpers.ToBitmapSource(imgDisplay); break;
             }
@@ -228,7 +314,7 @@ namespace BallCheatARKinect
             txtLog.AppendText(tmpt);
         }
 
-        #region win func
+        #region Window function
         private void frmMain_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             if (sensor != null) sensor.Stop();
@@ -252,9 +338,16 @@ namespace BallCheatARKinect
             sensor.AllFramesReady -= sensor_AllFramesReady;
             //Close();
             if (sensor != null) sensor.Stop();
+            GC.Collect();
             Application.Current.Shutdown();
         }
         #endregion
+
+
+        private void sldKinectTilt_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            sensor.ElevationAngle = (int)sldKinectTilt.Value;
+        }
 
         private void btnDisplay_Click(object sender, RoutedEventArgs e)
         {
@@ -271,7 +364,8 @@ namespace BallCheatARKinect
         /// <param name="e"></param>
         private void imgOutMain_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Point mPos = e.GetPosition(imgOutMain);
+            System.Windows.Point mPos = e.GetPosition(imgOutMain);
+            Point mPosF = new Point((float)mPos.X, (float)mPos.Y);
 
             if(isBallDepthSetting)
             {
@@ -291,18 +385,20 @@ namespace BallCheatARKinect
             {
                 if(poolPosSetting == 1)
                 {
-                    poolPos[0] = mPos;
+                    poolPos[0] = WPoint2DPoint(mPosF);
                     lblPoolPos1Val.Text = mPos.ToString();
                     lblPoolPos1Val.FontWeight = FontWeights.Normal;
                     poolPosSetting = 2;
                 }
                 else if(poolPosSetting == 2)
                 {
-                    poolPos[1] = mPos;
+                    poolPos[1] = WPoint2DPoint(mPosF);
                     //Calculate poolPos's width and height
                     poolWidth = (int)Math.Abs(poolPos[1].X - poolPos[0].X);
                     poolHeight = (int)Math.Abs(poolPos[1].Y - poolPos[0].Y);
                     lblPoolPos2Val.Text = mPos.ToString();
+                    //Set imgDisplay's Size
+                    imgDisplay.Resize(poolWidth, poolHeight, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR);
                     lblPoolPos2Val.FontWeight = FontWeights.Normal;
                     poolPosSetting = 0;
                 }
@@ -310,16 +406,18 @@ namespace BallCheatARKinect
         }
         private void imgOutMain_MouseMove(object sender, MouseEventArgs e)
         {
-            Point mPos = e.GetPosition(imgOutMain);
+            System.Windows.Point mPos = e.GetPosition(imgOutMain);
             if (isBallDepthSetting)
             {
-                ballDepth = depthPixels[(int)(mPos.X + (mPos.Y * 640))].Depth;
+                if ((int)(mPos.X + (mPos.Y * 640)) < depthPixels.Length) 
+                    ballDepth = depthPixels[(int)(mPos.X + (mPos.Y * 640))].Depth;
                 lblBallDepthVal.Text = ballDepth.ToString();
                 lblBallDepthVal.FontWeight = FontWeights.Bold;
             }
             else if (isPoolDepthSetting)
             {
-                poolDepth = depthPixels[(int)(mPos.X + (mPos.Y * 640))].Depth;
+                if ((int)(mPos.X + (mPos.Y * 640)) < depthPixels.Length)
+                    poolDepth = depthPixels[(int)(mPos.X + (mPos.Y * 640))].Depth;
                 lblPoolDepthVal.Text = poolDepth.ToString();
                 lblPoolDepthVal.FontWeight = FontWeights.Bold;
             }
@@ -335,7 +433,6 @@ namespace BallCheatARKinect
         {
             poolDepthRange = (int)((Slider)sender).Value;
         }
-
         private void sldBallDepthRange_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             ballDepthRange = (int)((Slider)sender).Value;
@@ -366,6 +463,47 @@ namespace BallCheatARKinect
         {
             isPoolDepthSetting = true;
         }
+        #endregion
+
+        /// <summary>
+        /// Output Image Button Warping Adjust Method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOutWrap_Click(object sender, RoutedEventArgs e)
+        {
+            string tag = ((Button)sender).Tag.ToString();
+            string pos = tag.Substring(0, 2);
+            char dir = tag[2];
+            System.Drawing.PointF nextPos;
+            int pointNum = 0;
+            switch(pos)
+            {
+                case "LT": pointNum = 0; break;
+                case "RT": pointNum = 1; break;
+                case "RB": pointNum = 2; break;
+                case "LB": pointNum = 3; break;
+            }
+            nextPos = dest[pointNum];
+
+            switch(dir)
+            {
+                case 'U': nextPos.Y -= nextPos.Y > 0 ? 1 : 0; break;
+                case 'R': nextPos.X += nextPos.X < poolWidth-1 ? 1 : 0; break;
+                case 'D': nextPos.Y += nextPos.Y < poolHeight-1 ? 1 : 0; break;
+                case 'L': nextPos.X -= nextPos.X > 0 ? 1 : 0; break;
+            }
+
+            dest[pointNum] = nextPos;
+
+            mywarpmat = CameraCalibration.GetPerspectiveTransform(srcs, dest);
+        }
+
+        #region Drawing.Point Windows.Point Converter
+        System.Drawing.Point WPoint2DPoint(Point p)
+        { return new System.Drawing.Point((int)p.X, (int)p.Y);}
+        Point DPoint2WPoint(System.Drawing.Point p)
+        { return new Point(p.X, p.Y); }
         #endregion
     }
 }
